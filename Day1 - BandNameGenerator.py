594ef4b5926564620960425c1eceff84fd5372b3
ljmagudelo <ljmagudelo@gmail.com>
2022-04-18 22:23:15 -0500

    Add files via upload
    Day 1
    https://replit.com/@appbrewery/band-name-generator-end

---
+++ Day1 - BandNameGenerator.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# 4/18/2022
+# Leonardo Agudelo
+# Day 1, First Proyect - Band Name Generator
+
+""" Code used at first but changed and improved
+name1 = "Los "
+name2 = input("Descripcion de su grupo en una palabra buena o mala\nEjemplo: Carimasticos, encantadores, buenas personas: ")
+name3 = " de "
+name4 = input("Ciudad mas caliente que se venga a tu mente: ")
+name5 = name1 + name2 + name3+ name4
+print(f"\nEl nombre de tu banda coleta podria ser {name5}")
+"""
+
+WelcomeMessage = "Hello World\n"
+WelcomeMessage += "Bienvenidos a mi primer proyecto\n"
+WelcomeMessage += "The Coleto's Band Name generator o en español\n"
+WelcomeMessage += "Generador de nombres para bandas coletas\n"
+WelcomeMessage += "Emepezemos......"
+print(WelcomeMessage)
+name1= "Los "
+name1+= input("Descripcion de su grupo en una palabra buena o mala\nEjemplo: Carimasticos, encantadores, buenas personas: ")
+name1+= " de "
+name1+= input("Ciudad mas caliente que se venga a tu mente: ")
+print(f"\nEl nombre de tu banda coleta podria ser {name1}")
